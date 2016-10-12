1523cc3575460e2fb9adc00147c6d26f73ae0575
Steve_James <steven.rayno@kearsarge.org>
2016-10-12 12:21:51 -0400

    fixed...i hope...

---
+++ story3.py
@@ -0,0 +1,12 @@
+from input import *
+
+#By James_Gachelin
+
+def story():
+    Name1 = getWord("Enter a name ")
+    
+    text = " "
+    text += "Once appon a time " + Name1 
+    text += "There was a boy called " + Name1
+    
+    return text
