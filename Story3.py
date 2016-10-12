60e7e1e2eaf75bf4577e702ce171c008ccff15d3
James1309 <James.gachelin@kearsarge.org>
2016-10-12 11:37:25 -0400

    Edits

--- Story3.py
+++ Story3.py
@@ -6,7 +6,7 @@ def Story():
     Name1 = getWord("Enter a name ")
     
     text = " "
-    text += "Once appon a time " + Name1
+    text += "Once appon a time " + Name1 
     text += "There was a boy called " + Name1
     
     return text
